18ee014324e7c06f6f7e61176a9eacdf865e57e6
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-02 20:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
  function daysOfAYear(year){
 	  if((year%4===0 && year%100 !== 0) || (year%400===0)){
-		  console.log(366);
+		  return 366;
 	  }
 	 else{
-		 console.log(365)
+		 return 365;
 	 }
  }
